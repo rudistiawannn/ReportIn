3237deffa3a369b586b87042811bebea3d8efa24
programmermedical <programmermedical@gmail.com>
2023-11-30 05:03:37 +0700

    update progress backend #1

--- src/index.js
+++ src/index.js
@@ -6,15 +6,22 @@
 // eslint-disable-next-line eol-last, no-unused-vars
 
 const express = require('express');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const { PrismaClient } = require('@prisma/client');
 
+dotenv.config();
+const prisma = new PrismaClient();
 const app = express();
-
 // const PORT = process.env.PORT;
 const PORT = 5000;
 
 app.get('/', (req, res) => {
   res.send('Ini halaman utama');
+});
+
+app.get('/berita', async (req, res) => {
+  const news = await prisma.reportData.findMany();
+  res.send(news);
 });
 
 app.listen(PORT, () => {
