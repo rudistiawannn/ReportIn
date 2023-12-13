011a804ce10a3a41a7643e3ce12734ff28371db1
rudisetiawan <rudistiawannn@gmail.com>
2023-12-13 15:10:59 +0800

    .

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
-// const PORT = 5000;
+// const PORT = process.env.PORT;
+const PORT = 5000;
 
 app.use(express.json());
 
