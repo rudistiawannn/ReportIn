74ae8ecbc367b0a64852220e9486402764deaee7
rudisetiawan <rudistiawannn@gmail.com>
2023-12-14 22:12:39 +0800

    .

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,13 +6,12 @@ const auth = require('../service/auth.service');
 class authController {
   static register = async (req, res, next) => {
     try {
-      // const user = await auth.register(req.body);
-      // res.status(200).send({
-      //   status: true,
-      //   message: 'User created successfully',
-      //   data: user,
-      // });
-      req.send('oke');
+      const user = await auth.register(req.body);
+      res.status(200).send({
+        status: true,
+        message: 'User created successfully',
+        data: user,
+      });
     } catch (e) {
       next(createError(e.statusCode, e.message));
     }
