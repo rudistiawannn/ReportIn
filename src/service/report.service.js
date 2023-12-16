8993d0b07a1c857b9c37ad2da3df510d69f68ca4
rudisetiawan <rudistiawannn@gmail.com>
2023-12-16 15:56:34 +0800

    .

--- src/service/report.service.js
+++ src/service/report.service.js
@@ -49,7 +49,7 @@ const updatedReportById = async (newData, reportId) => {
 
 const diskStorage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, path.join(__dirname, '../../../../../assets'));
+    cb(null, path.join(__dirname, '../../../../../tmp/assets'));
   },
   filename(req, file, cb) {
     cb(
