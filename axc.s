8fee2025a2e5d6fe348cbff7ccf52241489c5881
rudisetiawan <rudistiawannn@gmail.com>
2023-12-17 17:13:30 +0800

    .

---
+++ axc.s
@@ -0,0 +1,340 @@
+[33mcommit d0041a630648118462cd0f983775d2800ba68d69[m[33m ([m[1;36mHEAD -> [m[1;32mBack-End-#2[m[33m)[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Sat Dec 16 16:37:05 2023 +0800
+
+    .
+
+[33mcommit 40e0a219e29db76e8ec41ee9a2032d98a9b2e226[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Sat Dec 16 16:00:27 2023 +0800
+
+    ,.
+
+[33mcommit 8993d0b07a1c857b9c37ad2da3df510d69f68ca4[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Sat Dec 16 15:56:34 2023 +0800
+
+    .
+
+[33mcommit 968163599a7f794f9ceaf5438f350b0e2a293388[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Sat Dec 16 15:56:20 2023 +0800
+
+    .
+
+[33mcommit 2f4fd5f9b9be3388b9e3f4f5025b87b295486e01[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Sat Dec 16 15:34:20 2023 +0800
+
+    .
+
+[33mcommit 76fc65b8eef9c8223e601cb68deb742f62a98481[m
+Author: myahyaayyas <92872659+myahyaayyas@users.noreply.github.com>
+Date:   Fri Dec 15 15:05:08 2023 +0700
+
+    login dan register
+
+[33mcommit 74ae8ecbc367b0a64852220e9486402764deaee7[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 22:12:39 2023 +0800
+
+    .
+
+[33mcommit a9325fc820589250bbe5a8ee967e80158dfc9e22[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 22:09:31 2023 +0800
+
+    .
+
+[33mcommit 7f0922c77673ed2d1aef9f45660ebd694f9ca740[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 21:51:30 2023 +0800
+
+    .
+
+[33mcommit d89f48d3ed7a0a65944454281060d4220438d98e[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 21:44:13 2023 +0800
+
+    .
+
+[33mcommit a90b04409fb605f5f161567d76b3ce219e435280[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 20:59:56 2023 +0800
+
+    .
+
+[33mcommit 3f393d70ca44fc8c30dff34afd92687a014907d9[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 20:53:22 2023 +0800
+
+    .
+
+[33mcommit 10885085fd9c52fbbfe742ddbf6572808fd92dd7[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 20:17:43 2023 +0800
+
+    .
+
+[33mcommit 320cf7d3145b4baa889e82bab9a45e4cd8b5578e[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 20:11:37 2023 +0800
+
+    .
+
+[33mcommit ddb9d183c0128b5f6721445b5563bcbc09eca08a[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 19:59:34 2023 +0800
+
+    .
+
+[33mcommit fde67e50338a5e3c7f2309a100e37763142bebe2[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 16:54:50 2023 +0800
+
+    .
+
+[33mcommit 5bea082283950249675f4b124ac4f1853a25e266[m
+Author: rudisetiawan <rudistiawannn@gmail.com>
+Date:   Thu Dec 14 16:25:03 2023 +0800
+
+    .
+
+[33mcommit b4ec969f8e472b31c8df5b6590424c2d43f6bfd4[m
+Author: rudistiawannn <95742757+rudistiawannn@users.noreply.github.com>
+Date:   Thu Dec 14 15:53:23 2023 +0800
+
+    Update vercel.json
+
+[33mcommit 618b30e025920d9249253d83a9cf5ec50ec0ee52[m
+Merge: 54e1f6e d4ee6bf
+Author: Zainal Saputra <101336276+programmermedical@users.noreply.github.com>
+Date:   Thu Dec 14 14:40:08 2023 +0700
+
+    Merge pull request #17 from programmermedical/main
+    
+    add vercel
+
+[33mcommit d4ee6bfd6aae975668203d8052533ca67d4ad795[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Thu Dec 14 14:39:01 2023 +0700
+
+    add vercel app
+
+[33mcommit 54e1f6ebf5b05dba0424cbc3a6fb3aa5a4096af9[m
+Merge: cdcfbfa 780aea1
+Author: Zainal Saputra <101336276+programmermedical@users.noreply.github.com>
+Date:   Thu Dec 14 14:37:32 2023 +0700
+
+    Merge pull request #16 from programmermedical/main
+    
+    update deployment Back-End #2
+
+[33mcommit 780aea183ed8e06d468f3051345a7723cb1a7668[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Thu Dec 14 14:35:14 2023 +0700
+
+    update routes & migrations
+
+[33mcommit cdcfbfa5b79a1bed21a5ea47aab750415f66ed9e[m
+Merge: 46a9dbc 4593eaf
+Author: Zainal Saputra <101336276+programmermedical@users.noreply.github.com>
+Date:   Thu Dec 14 14:34:32 2023 +0700
+
+    Merge pull request #15 from programmermedical/main
+    
+    update deployment back-end
+
+[33mcommit 4593eaf9af772d30f338c2ed2c5f2255a990a921[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Thu Dec 14 14:27:54 2023 +0700
+
+    update routes & migrations
+
+[33mcommit 97b58ac474e056ed61412854c6e4da4592143156[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Thu Dec 14 14:11:43 2023 +0700
+
+    compare routes and migrations
+
+[33mcommit 423d9d0bf4004ab4852e201f185ff7fceeff9c78[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Thu Dec 14 13:26:30 2023 +0700
+
+    update routes register
+
+[33mcommit 1fcc9e1b58fd80aa9320a109012fb1a35687f92a[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Mon Dec 11 02:36:09 2023 +0700
+
+    create register login feature & destructure directory architecture
+
+[33mcommit 1384158a34d27a4333f4a5f008d332a77319fbc9[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Sun Dec 10 05:44:08 2023 +0700
+
+    create feature role user
+
+[33mcommit cef1fdf527ce747eae7237a10926cf44dfd33731[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Sat Dec 9 23:04:07 2023 +0700
+
+    update files name &  structure directory
+
+[33mcommit bcb419b19cb8e73561bd4fc9717b13e98657e7b2[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Sat Dec 9 23:01:10 2023 +0700
+
+    update files name &  structure directory
+
+[33mcommit f407ca38a14be37874a6fbe4da8f7784296ae5bc[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Thu Dec 7 04:48:30 2023 +0700
+
+    update error handling
+
+[33mcommit ec7c5936999bc80c170897705f3f3aa1fa337b2b[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Mon Dec 4 00:47:51 2023 +0700
+
+    update resource news API
+
+[33mcommit 28eb3358f320a84de5c6e14f185e80e5fd5c72bf[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Sat Dec 2 05:34:56 2023 +0700
+
+    update progress backend #4
+
+[33mcommit fe0cb0d567d01b173388298f1c16e912e430114f[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Sat Dec 2 05:33:38 2023 +0700
+
+    update progress backend #4
+
+[33mcommit 6175f7da93c2b139e1fc70a9fc0359c6ffd57c76[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Sat Dec 2 04:46:10 2023 +0700
+
+    update progress backend #4
+
+[33mcommit 8363702297471848576bb7c1d5e508f3c9a57803[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Fri Dec 1 05:22:38 2023 +0700
+
+    update progress backend #3
+
+[33mcommit 1654b6046f434b21ad0a70e2faa137b45ed63b20[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Fri Dec 1 05:21:20 2023 +0700
+
+    update progress backend #3
+
+[33mcommit 298d0d7b89844f3194ccbc816e6a9a2060ef53af[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Fri Dec 1 05:13:05 2023 +0700
+
+    update progress backend #3
+
+[33mcommit cfacb4801bb2deff8b20b800403156a3750f763b[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Fri Dec 1 04:18:36 2023 +0700
+
+    update progress backend #2
+
+[33mcommit 3237deffa3a369b586b87042811bebea3d8efa24[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Thu Nov 30 05:03:37 2023 +0700
+
+    update progress backend #1
+
+[33mcommit ac7f76bad4e6a2e22de46f6a557ae9f78c4c6b4a[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Thu Nov 30 03:18:28 2023 +0700
+
+    create template Back-End
+
+[33mcommit 58c194a15b630731ff9245b2a725b68b9553f2e5[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Thu Nov 30 02:32:13 2023 +0700
+
+    create template Back-End
+
+[33mcommit a302753eb0258000be4c4e0c0bdbbfc952f2dbef[m
+Merge: 56d592f 3340c86
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Wed Nov 29 20:35:17 2023 +0700
+
+    Merge branch 'main' of https://github.com/programmermedical/reportin-app-back-end
+
+[33mcommit 56d592ffc6ba46ad77aed32882d4bf85a1c288d9[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Wed Nov 29 20:34:28 2023 +0700
+
+    update index forking
+
+[33mcommit 3340c86f9bf9b0de868b871876f5971118dea5d7[m
+Merge: 8704a06 93ffa54
+Author: Programmer Medical <101336276+programmermedical@users.noreply.github.com>
+Date:   Wed Nov 29 19:31:11 2023 +0700
+
+    Merge branch 'Back-End' into main
+
+[33mcommit 8704a06ff56a2ee1f292decfc8de7738d2082fa5[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Wed Nov 29 19:15:40 2023 +0700
+
+    readme update progress
+
+[33mcommit 93ffa543bd5404898d71dfabe46d72aa02eec59a[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Wed Nov 29 18:47:52 2023 +0700
+
+    readme update progress
+
+[33mcommit faf35238ec2b092b3e0c4b1763598231a6033ce2[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Wed Nov 29 18:44:05 2023 +0700
+
+    readme update progress
+
+[33mcommit de924b57583313c301595b013dcbb5e1a237cba7[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Wed Nov 29 18:37:07 2023 +0700
+
+    remote testing
+
+[33mcommit 46a9dbc6e1bfa75c786cff3613807a71ef425f83[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mmain[m[33m)[m
+Author: programmermedical <programmermedical@gmail.com>
+Date:   Wed Nov 29 18:35:46 2023 +0700
+
+    remote testing
+
+[33mcommit 36f96823cbd20f0d18b7ec712cd9fc7f949b2832[m
+Author: Rizkyooo <ainurrizkyooo@gmail.com>
+Date:   Sat Nov 18 13:28:25 2023 +0800
+
+    delete
+
+[33mcommit 3d8ad58aa3c8b72423db568829d70329e5bb73e3[m
+Author: Rizkyooo <ainurrizkyooo@gmail.com>
+Date:   Sat Nov 18 13:25:57 2023 +0800
+
+    testtt
+
+[33mcommit 9178c25bcaafc3abe195fc7c23116deea8a0fd9d[m
+Author: Muhammad Ainur Rizki <108166334+Rizkyooo@users.noreply.github.com>
+Date:   Tue Nov 14 00:15:12 2023 +0800
+
+    test
+
+[33mcommit 7bf998547aefa431d0360a7470a8c7e863a880d6[m
+Author: Muhammad Ainur Rizki <108166334+Rizkyooo@users.noreply.github.com>
+Date:   Tue Nov 14 00:12:04 2023 +0800
+
+    test
+
+[33mcommit fcaf19384ad750b6a9c2301e7ffabd05a4b7d004[m
+Author: rudistiawannn <95742757+rudistiawannn@users.noreply.github.com>
+Date:   Mon Nov 13 23:00:09 2023 +0800
+
+    Initial commit
