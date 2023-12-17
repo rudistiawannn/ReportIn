833d2acbef703e2583067a7f24011748777f7a3c
Zainal Saputra <101336276+programmermedical@users.noreply.github.com>
2023-12-17 14:43:13 +0700

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -9,11 +9,7 @@ const auth = require('../service/auth.service');
 class authController {
   static register = async (req, res, next) => {
     try {
-      const user = await auth.register(uuidv4(), req.body);
-      res.status(200).send({
-        status: true,
-        message: 'User created successfully',
-        data: user,
+      res.send('oke');
       });
     } catch (e) {
       res.send('errno');
