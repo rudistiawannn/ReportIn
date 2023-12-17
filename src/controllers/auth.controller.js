72d151d00ef9b4313a56460f224dfedb0623d7e0
rudisetiawan <rudistiawannn@gmail.com>
2023-12-17 17:24:22 +0800

    mmmm

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,40 +1,32 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/no-extraneous-dependencies */
-// eslint-disable-next-line import/order
-const { v4: uuidv4 } = require('uuid');
-// const cookieSession = require('cookie-session');
-const jwt = require('../utils/jwt');
+const createError = require('http-errors');
 const auth = require('../service/auth.service');
 
 class authController {
   static register = async (req, res, next) => {
     try {
-      res.send('oke');
+      const user = await auth.register(req.body);
+      res.status(200).send({
+        status: true,
+        message: 'User created successfully',
+        data: user,
       });
     } catch (e) {
-      res.send('errno');
+      next(createError(e.statusCode, e.message));
     }
   };
 
   static login = async (req, res, next) => {
     try {
       const data = await auth.login(req.body);
-      const accessToken = await jwt.signAccessToken(data, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-      res.cookie('accessToken', accessToken, {
-        httpOnly: true,
-      });
       res.status(200).send({
         status: true,
         message: 'Account login successful',
         data,
-        accessToken,
       });
     } catch (e) {
-      next(res.status(500).json({
-        status: 'error',
-        code: 500,
-        message: 'Internal Server Error',
-      }));
+      next(createError(e.statusCode, e.message));
     }
   };
 
@@ -47,11 +39,7 @@ class authController {
         data: users,
       });
     } catch (e) {
-      next(res.status(500).json({
-        status: 'error',
-        code: 500,
-        message: 'Internal Server Error',
-      }));
+      next(createError(e.statusCode, e.message));
     }
   };
 }
