d92ff9c2a7aeadf35cd228a766eb05e8b1b1a57c
rudisetiawan <rudistiawannn@gmail.com>
2023-12-18 11:58:46 +0800

    prodnew

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,23 @@
-const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    open: true,
-    port: 9000,
-    client: {
-      overlay: {
-        errors: true,
-        warnings: true,
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
       },
-    }, 
+    ],
   },
 });
