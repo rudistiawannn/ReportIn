7238459625f305c5a5d83d734d0fe080f7cad6e4
programmermedical <programmermedical@gmail.com>
2023-12-18 18:26:59 +0700

    debugging #4

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -23,19 +23,19 @@ class AuthService {
   }
 
   static async login(data) {
-    return data;
-    // const user = await prisma.user.findFirst({
-    //   where: {
-    //     email,
-    //   },
-    // });
+    const { email } = data;
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
     // if (!user) {
     //   throw createError.NotFound('User not registered');
     // }
     // const checkPassword = bcrypt.compareSync(password, user.password);
     // if (!checkPassword) throw createError.Unauthorized('Email address or password not valid');
     // delete user.password;
-    // return user;
+    return user;
   }
 
   static async all() {
