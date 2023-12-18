f6d9c79fffd78208995a84e653edf217698b62c1
programmermedical <programmermedical@gmail.com>
2023-12-18 18:25:18 +0700

    debugging #4

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -23,19 +23,19 @@ class AuthService {
   }
 
   static async login(data) {
-    const { email, password } = data;
-    const user = await prisma.user.findFirst({
-      where: {
-        email,
-      },
-    });
-    if (!user) {
-      throw createError.NotFound('User not registered');
-    }
-    const checkPassword = bcrypt.compareSync(password, user.password);
-    if (!checkPassword) throw createError.Unauthorized('Email address or password not valid');
-    delete user.password;
-    return user;
+    return data;
+    // const user = await prisma.user.findFirst({
+    //   where: {
+    //     email,
+    //   },
+    // });
+    // if (!user) {
+    //   throw createError.NotFound('User not registered');
+    // }
+    // const checkPassword = bcrypt.compareSync(password, user.password);
+    // if (!checkPassword) throw createError.Unauthorized('Email address or password not valid');
+    // delete user.password;
+    // return user;
   }
 
   static async all() {
