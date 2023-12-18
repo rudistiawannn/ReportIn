6841704419f8a11f6106ff205308bf5bfba635e8
programmermedical <programmermedical@gmail.com>
2023-12-18 19:36:28 +0700

    fixing #4

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -9,7 +9,7 @@ const auth = require('../service/auth.service');
 class authController {
   static register = async (req, res, next) => {
     try {
-      const user = await req.body;
+      const user = await auth.register(req.body);
       res.status(200).send({
         status: true,
         message: 'User created successfully',
