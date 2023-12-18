59dccb9462f52b2d33339a8260fc01ddee1ec74b
programmermedical <programmermedical@gmail.com>
2023-12-18 17:23:40 +0700

    update auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,9 +8,9 @@ const jwt = require('../utils/jwt');
 const auth = require('../service/auth.service');
 
 class authController {
-  static register = async (req, res, next) => {
+  static register = (req, res, next) => {
     try {
-      const user = await auth.register(uuidv4(), req.body);
+      const user = auth.register(uuidv4(), req.body);
       res.status(200).send({
         status: true,
         message: 'User created successfully',
